5f060e41e928dcee9bcfde40aba5f99c5b4ca058
Simon Liang <simon@divby0.io>
2016-06-02 15:43:27 +0800

    skeleton

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+var mainState = {
+  preload: function() {
+
+  },
+  create: function() {
+
+  },
+  update: function() {
+
+  },
+  render: function() {
+
+  }
+};
+var game = new Phaser.Game(400, 490);
+game.state.add('main', mainState);
+game.state.start('main');
